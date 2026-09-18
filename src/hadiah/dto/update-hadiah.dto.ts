81c65efb058cef43101aa9a401960dd4a6d4fa6d
gesyadila <gesyadila@gmail.com>
2026-09-18 19:18:38 +0700

    Fix hadiah multipart number validation

--- src/hadiah/dto/update-hadiah.dto.ts
+++ src/hadiah/dto/update-hadiah.dto.ts
@@ -3,17 +3,20 @@ import {
   IsNotEmpty,
   Min,
 } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class UpdateHadiahDto {
   @IsNotEmpty()
   namaHadiah: string;
 
   @IsNotEmpty()
+  @Type(() => Number)
   @IsInt()
   @Min(0)
   poinDibutuhkan: number;
 
   @IsNotEmpty()
+  @Type(() => Number)
   @IsInt()
   @Min(0)
   stok: number;
